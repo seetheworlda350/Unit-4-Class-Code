d15429849d64ad22bc7087a00506506a5059714b
smithbwlhs <smithb@wlwv.k12.or.us>
2024-10-31 14:43:10 -0700

    done for the day

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,4 +1,5 @@
 package lesson1;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +8,18 @@ public class Main {
     }
 
     public static void guessMyNumber(){
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("I'm thinking of a number between 1 and 10.");
+        System.out.println("See if you can guess the number!");
+        int secretNumber = (int)(Math.random()*10+1);
+        // for debugging System.out.println("Test: "+secretNumber);
+        System.out.print("Enter your guess: ");
+        int userGuess = input.nextInt();
+        while(secretNumber != userGuess){
+            System.out.println("Try again!");
+            System.out.print("Enter your guess: ");
+            userGuess = input.nextInt();
+        }
+        System.out.println("Correct!");
     }
 }
