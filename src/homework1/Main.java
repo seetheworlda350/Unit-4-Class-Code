f9b1768593b102f5d98c6d2b5f9aa90e82feacb3
smithbwlhs <smithb@wlwv.k12.or.us>
2024-11-14 11:54:35 -0800

    updated

---
+++ src/homework1/Main.java
@@ -0,0 +1,43 @@
+package homework1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        guessingGame();
+        tiredTurtle();
+        notATamagotchi();
+
+    }
+
+    /**
+     * Picks a random number between 1 and 100 and asks the user
+     * to guess it. For each guess, the program should tell the user whether
+     * the guess is too high, too low, or correct.
+     *
+     */
+    public static void guessingGame(){
+        //feel free to use the Random class
+        System.out.println("To be done...");
+    }
+
+    /**
+     * Tired Turtle
+     * Asks the user how many steps they want the turtle to take in its
+     * first move. Then it calculates and displays how many total steps
+     * the turtle took until it stopped.
+     *
+     */
+    public static void tiredTurtle(){
+        System.out.println("To be done...");
+    }
+
+
+
+    /**
+     * Write your own description
+     */
+    public static void notATamagotchi(){
+        System.out.println("To be done...");
+    }
+
+}
